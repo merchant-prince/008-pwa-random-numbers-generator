a7b4a6cd8dbc1f9aeb3e3fef5625e38ee3ff9567
Harivansh <hello@harivan.sh>
2022-05-16 19:25:12 +0400

    tdd: app can be rendered

--- cypress/integration/example.test.ts
+++ cypress/integration/example.test.ts
@@ -1,7 +1,21 @@
 describe("Application", () => {
-  it("runs the application", () => {
+  it("renders the application", () => {
     cy.visit("/");
 
-    cy.get("body").should("contain.text", "Hello, World!");
+    cy.get('[data-test="random-numbers"]').should("be.visible");
+
+    [...Array(6).keys()].forEach((index) => {
+      cy.get(`[data-test="random-number-${index}"]`).should("be.visible");
+    });
+
+    cy.get('[data-test="controls"]').should("be.visible");
+
+    cy.get('[data-test="minimum-number"]').should("contain.value", 1);
+
+    cy.get('[data-test="maximum-number"]').should("contain.value", 40);
+
+    cy.get('[data-test="number-count"]').should("contain.value", 6);
+
+    cy.get('[data-test="regenerate-random-numbers"]').should("be.visible");
   });
 });
--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,20 @@
-<template>Hello, World!</template>
+<template>
+  <main>
+    <article data-test="random-numbers">
+      <section
+        v-for="index in [...Array(6).keys()]"
+        :key="index"
+        :data-test="`random-number-${index}`"
+      >
+        {{ index }}
+      </section>
+    </article>
+
+    <section data-test="controls">
+      <input type="number" data-test="minimum-number" value="1" />
+      <input type="number" data-test="maximum-number" value="40" />
+      <input type="number" data-test="number-count" value="6" />
+      <button data-test="regenerate-random-numbers">regenerate</button>
+    </section>
+  </main>
+</template>
