1b2046430f781c43507be5436a16951916651ae6
Harivansh <hello@harivan.sh>
2022-05-17 06:53:22 +0400

    fix: lg styles

--- src/App.vue
+++ src/App.vue
@@ -39,12 +39,14 @@ const regenerateRandomNumbers = () => {
 </script>
 
 <template>
-  <main class="w-full h-full flex flex-col items-center">
+  <main class="w-full h-full flex flex-col justify-center items-center">
     <article
-      class="flex-1 flex justify-center items-center"
+      class="flex-1 flex items-center lg:items-end"
       data-test="random-numbers"
     >
-      <div class="grid grid-cols-6 gap-2">
+      <div
+        class="grid grid-cols-6 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12 gap-2"
+      >
         <section
           v-for="(randomNumber, index) in randomNumbers"
           :key="index"
@@ -56,7 +58,10 @@ const regenerateRandomNumbers = () => {
       </div>
     </article>
 
-    <section class="w-80 px-3 py-2 flex justify-between" data-test="controls">
+    <section
+      class="w-80 sm:w-96 md:w-1/2 lg:w-1/3 px-3 py-2 flex lg:flex-1 justify-between lg:justify-top"
+      data-test="controls"
+    >
       <div class="flex space-x-4">
         <div class="flex items-center space-x-2">
           <input
